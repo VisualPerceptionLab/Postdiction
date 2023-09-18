3805c6ef20df3820e26675d1978099208fc661ef
Pieter Barkema <pbarkema@FIL.ION.UCL.AC.UK>
2023-09-18 11:58:32 +0100

    PRF scripts

---
+++ pRF_scripts_Essen/pRF_scripts_Essen/MRstim/MRstim/trunk/Displays/FIL_7T/displayParams.m
@@ -0,0 +1,17 @@
+function params = displayParams
+% Projector at 7T in Essen
+% Critical parameters
+params.numPixels = [1920 1200];
+params.dimensions = [35.5 22]; %[38.5 29.5]
+params.distance = 113;
+params.frameRate = 60;
+params.cmapDepth = 10;
+params.screenNumber = 0; %check in scanner
+% Descriptive parameters
+params.computerName = 'FIL_7T';
+params.monitor = 'Epson Projector';
+%params.card = 'RadeonMobility7500';
+%params.position = 'Jordan 474';
+
+% pixels per cm in 7T
+%mean(params.numPixels ./ params.dimensions)
---
+++ pRF_scripts_Essen/pRF_scripts_Essen/MRstim/MRstim/trunk/Displays/workstation/displayParams.m
@@ -0,0 +1,15 @@
+function params = displayParams
+% Projector at Skyra
+% ARW 022402
+% Critical parameters
+params.numPixels = [1680 1050];
+params.dimensions = [39 29];
+params.distance = 60;
+params.frameRate = 60;
+params.cmapDepth = 10;
+params.screenNumber = 0;
+% Descriptive parameters
+params.computerName = 'Workstation';
+params.monitor = 'Samsung monitor';
+%params.card = 'RadeonMobility7500';
+%params.position = 'Jordan 474';
