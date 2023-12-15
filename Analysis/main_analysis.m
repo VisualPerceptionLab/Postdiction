a82c3a26f3f6cd21c4258ea8b1509ae8be9c5be0
Pieter Barkema <pbarkema@FIL.ION.UCL.AC.UK>
2023-12-15 15:29:42 +0100

    update analysis pipeline

--- Analysis/main_analysis.m
+++ Analysis/main_analysis.m
@@ -1,8 +1,44 @@
 % here we could call betasanalysis.m and analyse_behavior to plot/analyse
 % all data.
+clear all; close all;
+subjects = {'Oliver_humanpilot','Oliver_humanpilot_copy_for_testing'};
 
-subjects = {'S09'};
-behaviour = analyse_behaviour(subjects);
+% How do I make sure this works for one and for many subjects?
+% The data structure is
+% subjects_behaviour{subject}{run}{block}.[trial_scores, confidences,
+% time_intervals]
+% Test this pipeline
+
+nSubjects = length(subjects);
+for iSubject=1:nSubjects
+    behaviour = analyse_behaviour(subjects{iSubject});
+    subjects_behaviour{iSubject} = behaviour;
+end
+
+% Make plots
+for iSubject=1:nSubjects
+    behaviour_data = subjects_behaviour{iSubject};
+    scores = behaviour_data.all_scores;
+    subplot(2, ceil(nSubjects/2), iSubject);
+    % plot scores
+    for iRun=1:length(behaviour_data)
+        % average over blocks
+        % alternatively average over runs and look at blocks
+
+        data{iRun} = behaviour_data
+    data{behaviour_data}
+    plot(data, 'LineWidth', 2);
+    
+    % Customize subplot title
+    title(behaviour_data);
+    
+    % Add other customization as needed
+    
+    % Example: Add labels, legend, etc. (replace with your own code)
+    xlabel('X-axis');
+    ylabel('Y-axis');
+    legend('Data');
+
 % Behavioral analysis
 % Plot the scores per block with average. 4 subbars are blocks, 4 bars are
 % conditions.
