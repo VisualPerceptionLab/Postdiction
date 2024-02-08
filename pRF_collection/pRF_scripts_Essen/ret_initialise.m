ae698a62481a6e986720ff4740805b943016fb5c
PBarkema <39303377+PBarkema@users.noreply.github.com>
2024-02-08 17:17:49 +0000

    update prf eye track

--- pRF_collection/pRF_scripts_Essen/ret_initialise.m
+++ pRF_collection/pRF_scripts_Essen/ret_initialise.m
@@ -11,4 +11,14 @@ pRF_paths = genpath(pRF_paths);
 addpath(pRF_paths);
 
 LoadPsychHID
+if Eyelink('Initialize')~=0; return; end % open a connection to the eyetracker PC
+eyename = ['prfeye', num2str(subjID)]
+Eyelink('Openfile',eyename)               % create test.edf on the eyetracker PC
+ret_val = Eyelink('StartRecording')      % start recording (to the file)
+
 ret
+
+Eyelink('StopRecording')                 % stop recording
+Eyelink('Closefile')                     % close the file
+Eyelink('ReceiveFile')                   % copy the file to the Stimulus PC
+Eyelink('Shutdown')                      % close the connection to the eyetracker PC
